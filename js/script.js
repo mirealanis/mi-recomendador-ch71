626d54b461e0b5b4021af18b0ad6757b8cb9ef7b
Mireya Alanis <alanisgmireya@gmail.com>
2026-08-23 21:04:01 -0600

    feat: agregar codigo a script

---
+++ js/script.js
@@ -0,0 +1,19 @@
+/*Guarda String - nombre*/
+const nombre = prompt("¿Cuál es tu nombre?", "Fulanito");
+alert("Hola, " + nombre);
+/*Guarda String - lugar recomendado*/
+const lugar = prompt("¿Puedes decirme un lugar que te guste?", "No recuerdo");
+/*Guarda number - edad*/
+const edad = Number(prompt("¿cuántos años tienes?"));
+/*Guarda number - numero de museos*/
+const numMuseos = Number(prompt("¿cuántos museos haz visitadon en este año?", 0));
+alert("¡Gracias por tus respuestas! ¡Bienvenido a mis recomendaciones!");
+/*Guarda boolean*/
+let ingreso = true;
+
+//Mostrar valores en consola
+console.log(nombre);
+console.log(lugar);
+console.log(edad);
+console.log(numMuseos);
+console.log(ingreso);
